0071f5350acf5a189e8caf615e353b4ce1106520
Yannick fumukani <slashcs7@hotmail.com>
2025-04-16 20:51:05 -0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Yannick Fumukani on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
